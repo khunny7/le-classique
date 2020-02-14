dfa9f77e1fa3c1a96d255f7dde8e1e0687394c58
Younghoon Gim <khunny7@live.com>
2020-02-13 20:04:56 -0800

    use book to show in firebase

--- src/data/book-repository.js
+++ src/data/book-repository.js
@@ -1,14 +1,24 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import { Book } from '../model/book'
 
 class BookRepository {
   constructor() {
     this.db = firebase.firestore();
   }
+
+  /**
+   * @returns { books } - array of object type Book
+   */
   get() {
     return this.db.collection("books").get().then((querySnapshot) => {
 
-      return querySnapshot.docs;
+      return querySnapshot.docs.map((querySnapshotDoc) => {
+        const book = new Book();
+        book.setQuerySnapshot(querySnapshotDoc);
+
+        return book;
+      });
     });
   }
 }
