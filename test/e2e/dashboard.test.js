974087030bb21245c8317eee2bcba08e8e273f85
Younghoon Gim <khunny7@live.com>
2020-04-04 20:51:27 -0700

    working on jest

--- test/e2e/dashboard.test.js
+++ test/e2e/dashboard.test.js
@@ -12,12 +12,19 @@ beforeAll(async () => {
   await page.goto('http://localhost:8080', { waitUntil: 'domcontentloaded' });
 });
 
-describe('Test header and title of the page', () => {
+describe('Dashboard page navigation', () => {
   test('Title of the page', async () => {
     const title = await page.title();
     expect(title).toBe('Getting Started 2');
 
     await page.waitForSelector('.jumbotron .welcome-user');
+    const welcomeUserContent = await page.evaluate()
+    $eval('.jumbotron > .welcome-user', (el) => {
+      console.log(el);
+      return el.value;
+    });
+    expect(welcomeUserContent).toBe('Hello, users!');
 
+    // console.log('expect is done');
   }, timeout);
 });
