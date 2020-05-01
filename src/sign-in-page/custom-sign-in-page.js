1b44a2aafdad886c53faf0623d16eaef99b897eb
Kate Sohng <kheeya7@hotmail.com>
2020-04-30 17:28:53 -0700

    log-in page design

--- src/sign-in-page/custom-sign-in-page.js
+++ src/sign-in-page/custom-sign-in-page.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import firebase from 'firebase/app';
 import {
-  Button, Form, ToggleButtonGroup, ToggleButton,
+  Button, Form, Container, Row, Col,
 } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 import './custom-sign-in-page.less';
@@ -10,14 +10,12 @@ const SignInPage = () => {
   const history = useHistory();
   const [emailAddress, setEmailAddress] = useState('');
   const [password, setPassword] = useState('');
-  const [signOption, setSignOption] = useState(1);
+
 
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth.languageCode = 'en';
     firebase.auth().signInWithPopup(provider).then((/* result */) => {
-
-
       history.push('/');
     }).catch((error) => {
       // Handle Errors here.
@@ -29,17 +27,17 @@ const SignInPage = () => {
     });
   };
 
-  const signUpWithEmailAndPassword = () => {
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(emailAddress, password)
-      .then(() => {
-        history.push('/');
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
+  // const signUpWithEmailAndPassword = () => {
+  //   firebase
+  //     .auth()
+  //     .createUserWithEmailAndPassword(emailAddress, password)
+  //     .then(() => {
+  //       history.push('/');
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // };
 
   const signInWithEmailAndPassword = () => {
     firebase
@@ -55,79 +53,94 @@ const SignInPage = () => {
   };
 
   return (
-    <div className="sign-in-page">
-      <div className="sign-in-section">
-        <div className="sign-in-main-contents">
-          <ToggleButtonGroup
-            type="radio"
-            name="sign-in-or-up"
-            value={signOption}
-            onChange={(val) => { console.log(val); setSignOption(val); }}
-          >
-            <ToggleButton value={1}>Sign-In</ToggleButton>
-            <ToggleButton value={2}>Sign-Up</ToggleButton>
-          </ToggleButtonGroup>
-          <div className="sign-in-up-section">
-            {
-              signOption === 1 &&
-              (
-                <div className="sign-in-container">
-                  <Form>
-                    <Form.Group controlId="formBasicEmail">
-                      <Form.Label>Email address</Form.Label>
-                      <Form.Control type="email" value={emailAddress} onChange={(e) => setEmailAddress(e.target.value)} />
-                    </Form.Group>
-
-                    <Form.Group controlId="formBasicPassword">
-                      <Form.Label>Password</Form.Label>
-                      <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                    </Form.Group>
-                    <Button
-                      className="sign-in-or-up-button"
-                      onClick={() => signInWithEmailAndPassword()}>
-                      Sign-In
-                    </Button>
-                  </Form>
-                </div>
-              )
-            }
-            {
-              signOption === 2 &&
-              (
-                <div className="sign-up-container">
-                  <Form>
-                    <Form.Group controlId="formBasicEmail">
-                      <Form.Label>Email address</Form.Label>
-                      <Form.Control type="email" value={emailAddress} onChange={(e) => setEmailAddress(e.target.value)} />
-                      <Form.Text className="text-muted">
-                        We will never share your email with anyone else.
-                      </Form.Text>
-                    </Form.Group>
-
-                    <Form.Group controlId="formBasicPassword">
-                      <Form.Label>Password</Form.Label>
-                      <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                    </Form.Group>
-                    <Button
-                      className="sign-in-or-up-button"
-                      onClick={() => signUpWithEmailAndPassword()}>
-                      Sign-Up
-                    </Button>
-                  </Form>
-                </div>
-              )
-            }
-          </div>
-          <div className="sign-in-with-button-container">
-            <Button className="sign-in-with-button" onClick={() => signInWithGoogle()}>
-              Sign in with Google Account
-            </Button>
-            <Button className="sign-in-with-button" onClick={() => signInWithGoogle()}>
-              Sign in with facebook Account
-            </Button>
-          </div>
+    <div className="log-in-page">
+      <div className="log-in-header">
+        <div className="head">
+          <a href="/">Placeholder - Logo</a>
         </div>
       </div>
+      <Container fluid>
+        <div className="log-in-content">
+          <Row>
+            <Col xs={12}>
+              <Button className="log-in-with-button google" onClick={() => signInWithGoogle()}>
+                Continue with Google
+              </Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <Button className="log-in-with-button facebook" onClick={() => signInWithGoogle()}>
+                Continue with facebook
+              </Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <div className="divider">
+                <strong className="divider-title">or</strong>
+              </div>
+            </Col>
+          </Row>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Control placeholder="Email address" type="email" value={emailAddress} onChange={(e) => setEmailAddress(e.target.value)} />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Control placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </Form.Group>
+            <Row>
+              <Col xs={12} sm={6}>
+                <div className="checkbox">
+                  <Form.Group controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Remember me" />
+                  </Form.Group>
+                </div>
+              </Col>
+              <Col xs={12} sm={6}>
+                <Button
+                  className="log-in-button"
+                  onClick={() => signInWithEmailAndPassword()}
+                >
+                  Log in
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+          <Row>
+            <Col>
+              <a href="/">Forgot your password?</a>
+            </Col>
+          </Row>
+          <div id="sign-up-section">
+            <Row>
+              <Col xs={12}>
+                <div className="divider" />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <h4>Don&apos;t have an account?</h4>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12}>
+                <Button className="log-in-with-button signup" onClick={() => signInWithGoogle()}>
+                  Sign up for Le-Classique
+                </Button>
+              </Col>
+            </Row>
+          </div>
+          <Row>
+            <Col xs={12}>
+              <div className="divider" />
+              <p className="textMuted">Message about terms &amp; conditions and Privacy policy</p>
+            </Col>
+          </Row>
+        </div>
+        {/* .login-content */}
+      </Container>
+      {/* .container */}
     </div>
   );
 };
